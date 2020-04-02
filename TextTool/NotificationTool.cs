b264f068ccc9dd1ae4e3dfee839ba69bef1a5538
yadewe <yadewe@live.com>
2020-04-02 16:13:04 +0800

    图标不会删除的问题，图标改成app的，加资源

--- TextTool/NotificationTool.cs
+++ TextTool/NotificationTool.cs
@@ -15,7 +15,8 @@ namespace TextTool
             var notification = new System.Windows.Forms.NotifyIcon()
             {
                 Visible = true,
-                Icon = System.Drawing.SystemIcons.Application,
+                // 在右下角显示的图标
+                Icon = TextTool.Properties.Resources.app,// 用App的图标
                 BalloonTipIcon = balloonTipIcon,
                 BalloonTipTitle = title,
                 BalloonTipText = tip,
@@ -24,20 +25,25 @@ namespace TextTool
 
             var showMinSeconds = showSeconds * 1000;
 
-            // 不确定主线程不等待，这个还会不会被执行
-            notification.BalloonTipClosed += (sender, args) => notification.Dispose();
-            notification.BalloonTipClicked += (sender, args) => notification.Dispose();
+            bool isClosed = false;
+            // 不确定主线程不等待，这个还会不会被执行（会有个图片在右下角那显示，不会自动取消）
+            notification.BalloonTipClosed += (sender, args) => { isClosed = true; MessageBox.Show("BalloonTipClosed " + DateTime.Now); };
+            notification.BalloonTipClicked += (sender, args) => { isClosed = true; MessageBox.Show("BalloonTipClicked " + DateTime.Now); };
             // Display for 5 seconds.
             notification.ShowBalloonTip(showMinSeconds);
 
-            //// This will let the balloon close after it's 5 second timeout
-            //// for demonstration purposes. Comment this out to see what happens
-            //// when dispose is called while a balloon is still visible.
-            //Thread.Sleep(showMinSeconds);
+            // This will let the balloon close after it's 5 second timeout
+            // for demonstration purposes. Comment this out to see what happens
+            // when dispose is called while a balloon is still visible.
+            var timeout = DateTime.Now.AddMilliseconds(showMinSeconds);
+            while(!isClosed && DateTime.Now < timeout)
+            {
+                Thread.Sleep(200);
+            }
 
-            //// The notification should be disposed when you don't need it anymore,
-            //// but doing so will immediately close the balloon if it's visible.
-            //notification.Dispose();
+            // The notification should be disposed when you don't need it anymore,
+            // but doing so will immediately close the balloon if it's visible.
+            notification.Dispose();
         }
     }
 }
